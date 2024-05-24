d5b341b6a3c8e62ac98c2e1dd0ad01fef5aa1967
jsb5324 <jsb5324@naver.com>
2024-05-24 15:08:04 +0900

    pythonbackend3

--- rewind/app.py
+++ rewind/app.py
@@ -1,13 +1,29 @@
-from flask import Flask, jsonify, request
-app = Flask(__name__)
-app.users = {}
-app.posts = []
-app.idCnt = 1
+# from flask import Flask, jsonify, request
+# app = Flask(__name__)
+# app.users = {}
+# app.posts = []
+# app.idCnt = 1
 
-@app.route('/sign-up', methods=['POST'])
-def signUp():
-    newUser = request.json
-    newUser['id'] = app.idCnt
-    app.users[app.idCnt] = newUser
-    app.idCnt += 1
-    return jsonify(newUser)
+# @app.route('/sign-up', methods=['POST'])
+# def signUp():
+#     newUser = request.json
+#     newUser['id'] = app.idCnt
+#     app.users[app.idCnt] = newUser
+#     app.idCnt += 1
+#     return jsonify(newUser)
+@app.route('/follow', methods=['post'])
+def follow():
+    payload = request.json
+    userId = int(payload['id'])
+    userIdToFollow = int(payload['follow'])
+
+    if userId not id app.users or userIdToFollow not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+    
+    user = app.users[userId]
+    if user.get('follow'):
+        user['follow'].append(userIdToFollow)
+        user['follow'] = list(set(user['follow']))
+    else:
+        user['follow'] = [userIdToFollow]
+    return jsonify(user)        
